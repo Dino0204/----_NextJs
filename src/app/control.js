544b2b93240eb5db7aac233bed533447d8610048
Dino0204 <s24020@gsm.hs.kr>
2024-11-13 14:17:18 +0900

    Fix: update 버그 수정

--- src/app/control.js
+++ src/app/control.js
@@ -1,24 +1,37 @@
 "use client";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export default function Control() {
+  const router = useRouter();
   const params = useParams();
   const id = params.id;
 
   return (
     <ul>
       <li>
-        <Link href="/create">create</Link>
+        <Link href="/create">Create</Link>
       </li>
 
       {id && (
         <>
           <li>
-            <Link href="/update/1">update</Link>
+            <Link href={`/update/${id}`}>Update</Link>
           </li>
           <li>
-            <input type="button" value="delete"></input>
+            <input
+              type="button"
+              value="Delete"
+              onClick={() => {
+                const options = { method: "DELETE" };
+                fetch(`http://localhost:9999/topics/${id}`, options)
+                  .then((res) => res.json)
+                  .then((result) => {
+                    router.push("/");
+                    router.refresh();
+                  });
+              }}
+            ></input>
           </li>
         </>
       )}
